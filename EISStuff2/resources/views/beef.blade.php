63d06f6e9093d0aba1acf5771c5815bf48da5c02
Newah <w0472168@nscc.ca>
2024-03-30 22:38:08 -0300

    Mar 30/24 2

--- EISStuff2/resources/views/beef.blade.php
+++ EISStuff2/resources/views/beef.blade.php
@@ -1,33 +1,32 @@
 {{--  *INSTALL BETTER COMMENTS EXTENSION ON VSCODE TO DISPLAY COMMENTS BETTER*
-  Beef Entry Form
-  By: Michele Roosje & Noah Taylor
-  Date: 3/29/24
-  Filename: beef.blade.php
-  ?Purpose: This is the beef entry form page for the Annapolis Valley Exhibition. This page is used to enter beef (cattle) into the competition.
+  *Beef Entry Form
+  *By: Michele Roosje & Noah Taylor
+  *Date: 3/29/24
+  *Filename: beef.blade.php
+  ? Purpose: This is the beef entry form page for the Annapolis Valley Exhibition. This page is used to enter beef (cattle) into the competition.
 
   *Changes by Noah*
-  - Added the extends for the beefLayout and nav
+  *- Added the extends for the beefLayout and nav
     ? I added the extends for the beefLayout and nav so that the page would have the same layout as the other pages
-  - Added the section for the head
+  *- Added the section for the head
     ? Included JS Script in head (beef.js) 
-  - Added the section for the title
-  - Added the section for the farm
-  - Added the section for the animal
+  *- Added the section for the title
+  *- Added the section for the farm
+  *- Added the section for the animal
     ? Added extra sections to better organize the page, as well as get rid of some of the <div><br><br></div> tags as @yield sections are auto spaced
-  
 
   *More Important Things*
-  ! - Moved JavaScript to beef.js
-  ! - Edited the content section
-  ! - Changed "star" to "red" for my CSS and my own sanity (ctrl + f -> red and then click the arrow, drop down type "star" click replace all if you want to change it back)
-  ! - Edits have been made in the original CSS file as well
-  ! - Changed some things due to my file structure/names of folders/files 
+    ! - Moved JavaScript to beef.js
+    ! - Edited the content section
+    ! - Changed "star" to "red" for my CSS and my own sanity (ctrl + f -> red and then click the arrow, drop down type "star" click replace all if you want to change it back)
+    ! - Edits have been made in the original CSS file as well
+    ! - Changed some things due to my file structure/names of folders/files 
 
   *TODO List*
-  TODO: 
+    TODO: 
     - PHP Connection to JavaScript
-      ? I was unsure how to do that completely, but I made a few edits to the JavaScript file so it may or may not work
-  TODO:
+        ? I was unsure how to do that completely, but I made a few edits to the JavaScript file so it may or may not work
+    TODO:
 --}}
 
 @extends('layouts.beefLayout')
@@ -51,34 +50,33 @@
 
 @section ('farm')
 <div class="wholeForm">
-  <table class="formTable text-center">
-  </table>    
-  <form action="" method="post">
-      <table class="formTable text-center entryInfo">
-          <thead>
-              <tr>
-                  <th></th>
-              </tr>
-          </thead>
-          <tbody>
-              <tr> 
-                  <td>
-                      <div class="row mb-3">
-                          <label for="insurance" class="col-sm-2 col-form-label textFieldLbl">Insurance</label>
-                      <div class="col-sm-4">
-                          <input type="text" class="form-control textFieldInp" id="insurance" placeholder="insurance #">
-                      </div>
-                  </div>
-              </td>
-              
-              <td>
-                <div class="row mb-3">
-                  <label for="farmNameInp" class="col-sm-2 col-form-label textFieldLbl">Farm Name</label>
-                  <div class="col-sm-4">
+    <table class="formTable text-center">
+    </table>    
+    <form action="" method="post">
+        <table class="formTable text-center entryInfo">
+            <thead>
+                <tr>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr> 
+                    <td>
+                    <div class="row mb-3">
+                            <label for="insurance" class="col-sm-2 col-form-label textFieldLbl">Insurance</label>
+                        <div class="col-sm-4">
+                            <input type="text" class="form-control textFieldInp" id="insurance" placeholder="insurance #">
+                        </div>
+                    </div>
+                    </td>
+        <td>
+            <div class="row mb-3">
+                    <label for="farmNameInp" class="col-sm-2 col-form-label textFieldLbl">Farm Name</label>
+                <div class="col-sm-4">
                     <input type="text" class="form-control textFieldInp" id="farmNameInp" placeholder="Farm Name">
-                  </div>
                 </div>
-              </td>
+            </div>
+        </td>
             </tr>
   
             <tr>
@@ -110,6 +108,8 @@
       <h3 class="heading3"><b>REGISTRATION NOT COMPLETE WITHOUT PROOF OF INSURANCE</b></h3>
     </div>
 
+    <h1>Animal 1</h1>
+
     {{--Animal Name--}}
     <label for="animalName">Name of Animal<span class="red">*</span></label>
     <input type="text" id="animalName" placeholder="Animal Name">
@@ -134,6 +134,55 @@
     <label for="dameName">Name of Dame</label>
     <input type="text" id="dameName" placeholder="Name of Dame">
   </div>
+
+  <div id="animalCheckbox">
+    <div class="checkbox-column">
+        <input type="checkbox" id="bullCalf">
+        <label for="bullCalf">1 Bull Calf - Born in 2023</label>
+        <br><br>
+
+        <input type="checkbox" id="steerCalf">
+        <label for="steerCalf">Prospect Steer Calf - Born in 2023</label>
+        <br><br>
+
+        <input type="checkbox" id="femaleIYearling">
+        <label for="femaleIYearling">Female Intermediate Yearling</label>
+        <p class="checkDesc">Born April 1/22 - December 31/22</p>
+
+        <input type="checkbox" id="twoYear">
+        <label for="twoYear">2 Year Cow / Calf</label>
+        <p class="checkDesc">Born January 1/21 - December 31/21, with her 2022 purebred, tattooed calf at foot</p>
+
+        <input type="checkbox" id="breedersHerd">
+        <label for="breedersHerd">Breeders Herd Group of 4</label>
+        <p class="checkDesc">(Male & Female) Bred by Exhibitor</p>
+
+        <input type="checkbox" id="showmanship">
+        <label for="showmanship">Open Beef Showmanship</label>
+    </div>
+    
+    <div class="checkbox-column">
+        <input type="checkbox" id="bullYearling">
+        <label for="bullYearling">Bull Yearling - Born in 2022</label>
+        <br><br>
+
+        <input type="checkbox" id="femaleCalf">
+        <label for="femaleCalf">Female Calf - Born in 2023</label>
+        <br><br>
+
+        <input type="checkbox" id="femaleSYearling">
+        <label for="femaleSYearling">Female Senior Yearling</label>
+        <p class="checkDesc">Born January 1/22 - March 31/22</p>
+        
+        <input type="checkbox" id="female">
+        <label for="female">Female</label>
+        <p class="checkDesc">Born prior to January 1/21 with 2023 purebred, tattooed calf at foot</p>
+
+        <input type="checkbox" id="sireGroup">
+        <label for="sireGroup">Get of Sire Group of 3</label>
+        <p>By same Sire (Male & Female)</p>
+    </div>
+</div>
 @endsection
 
 @section('content')
